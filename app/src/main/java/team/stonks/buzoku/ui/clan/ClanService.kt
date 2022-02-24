84c25e08f048ba97958ce8048cf2e1c975290d62
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-02-25 02:11:51 +0800

    clan Service volley hardcode

--- app/src/main/java/team/stonks/buzoku/ui/clan/ClanService.kt
+++ app/src/main/java/team/stonks/buzoku/ui/clan/ClanService.kt
@@ -1,7 +1,13 @@
 package team.stonks.buzoku.ui.clan
 
-import android.os.Handler
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import team.stonks.buzoku.leaderboards.Clan
+import team.stonks.buzoku.leaderboards.ClanMember
 
 class ClanService {
     /**
@@ -9,11 +15,38 @@ class ClanService {
      * @param id: The ID of the clan to get.
      * @param callback: The callback to call with the Clan object, once the network call is done.
      */
-    fun getClan(id: String, callback: (Clan) -> Unit) {
-        // TODO: Add actual network code here
-        val handler = Handler()
-        handler.postDelayed({
-            callback(Clan("Eastern Tigers"))
-        }, 1000)
+    fun getClan(clan_name: String, context : Context, callback: (Clan) -> Unit) {
+
+        val queue = Volley.newRequestQueue(context)
+        val url = "https://ninja-van-stonks.uc.r.appspot.com/calc_clan_scores/$clan_name"
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                Log.e("DEBUG", "request")
+
+                val clanScore = response.getDouble("clanScore")
+                   val members = response.getJSONObject("driver_rankings")
+                   var clanMembers = arrayListOf<ClanMember>()
+                   val memberKeys = members.names()
+                   for (i in 0..members.length()) {
+                       val key = memberKeys.getInt(i)
+                       val name = members.getString(key.toString())
+                       clanMembers.add(ClanMember(name, key))
+                   }
+                   val clan = Clan(clan_name, clanScore, clanMembers)
+                   Log.e("DEBUG", "CLAN $clan $clanMembers")
+               }
+            ,
+            { error ->
+                error.printStackTrace()
+            }
+        )
+        val mockArray = arrayListOf<ClanMember>(
+            ClanMember("Samuel", 80),
+            ClanMember("Dan", 20)
+        )
+        callback(Clan("Eastern Tigers", 69.0,mockArray))
+        queue.add(request)
+
     }
 }
